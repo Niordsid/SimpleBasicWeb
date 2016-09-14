5ee1d702a14367aca6201b49095c819c9f7d34ae
Niordsid <andriy_99@hotmail.com>
2016-09-14 14:50:49 -0500

    Revisando estados de las tareas.. falta agregar las funciones de showcomplete showincomplete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,47 @@
 
 var todos = [];
 
-
+/* funcion que agrega la tareas a la primera tabla */
 function showTodos(){
 
   $('#table2').html('');
 
   for(var i=0; i<todos.length; i++){
+    var button= "<button type='button' index="+i+" class='button-todo'> <span class='glyphicon glyphicon-ok'> </span> </button>";
+    var todo = "<tr> <td>"+ button+" </td>"+"<td>"+todos[i].name+"</td>"+"<td>"+todos[i].time+"</td>"+"</tr>";
 
-    var todo = "<tr index=>"+"<td>"+todos[i].done+"</td>"+"<td>"+todos[i].name+"</td>"+"<td>"+todos[i].time+"</td>"+"</tr>";
-    
 
     $('#table2').append(todo);
   }
+}
+
+/* funcion que muestra las tareas completas Nota: Falta terminarla*/
+function showcomplete(){
+  $('#completed-todo').html('');
+
+    for(var i=0; i<todos.length; i++){
+
+
+      var complete= "<tr> <td> </td> <td>"+todos[i].name+" </td> <td>"+todos[i].done+" </td> </tr>";
+      console.log(complete);
+      $('#completed-todo').append(complete)
+
+}
+
+}
+/* funcion que muestra las tareas incompletas Nota: Falta terminarla*/
+function showinclomplete(){
+  $('#incompleted-todo').html('');
+
+  for(var i=0; i<todos.length; i++){
+
+
+      var incomplete= "<tr> <td> </td> <td>"+todos[i].name+" </td> <td>"+todos[i].done+" </td> </tr>";
+      console.log(incomplete);
+      $('#incompleted-todo').append(incomplete)
+    }
+
+
 }
 
 $(document).ready(function(){
@@ -30,3 +59,27 @@ $(document).ready(function(){
     showTodos();
   });
 });
+
+
+
+$(document).on('click',".button-todo", function() {
+  var index= $(this).attr('index');
+
+  todos[index].done = !todos[index].done;
+  console.log(todos[index].done);
+
+
+/* Revisar el estado de la tarea */
+  if(todos[index].done ==true){
+
+    console.log(todos[index].name, todos[index].time ,todos[index].done);
+
+    /*showclomplete();*/
+  }else if (todos[index].done == false) {
+
+    console.log(todos[index].name, todos[index].time ,todos[index].done);
+    /*showincomplete();*/
+  }
+
+
+} );
