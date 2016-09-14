9b505f7d41140835e97196407e0bd1af4f9ea859
Niordsid <andriy_99@hotmail.com>
2016-09-13 22:35:04 -0500

    Toma de datos mediante JSON y guardarlos en un arreglo pra la impresion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,32 @@
-var todos=[];
+
+var todos = [];
+
 
 function showTodos(){
 
+  $('#table2').html('');
 
   for(var i=0; i<todos.length; i++){
 
-    var todo = "<tr>"+"<td>1</td>"+"<td>"+todos[i]+"</td>"+"<td>6 hours</td>"+"</tr>";
-    console.log(todo);
-  
+    var todo = "<tr index=>"+"<td>"+todos[i].done+"</td>"+"<td>"+todos[i].name+"</td>"+"<td>"+todos[i].time+"</td>"+"</tr>";
+    
+
     $('#table2').append(todo);
- }
+  }
 }
 
 $(document).ready(function(){
   $('#todo-button').click(function(){
-    todos.push($('#input-todo').val()); /* almacenar valor en el array todos */
-    $('#input-todo').val("");
-    console.log(todos);
+
+    /* Object JSON */
+    todo ={
+      done: false,
+      name:$('#input-todo').val(),
+      time:$('#time-task').val()
+    };
+
+    todos.push(todo); /* almacenar valor en el array todos */
+
     showTodos();
   });
 });
